a4a2fbbde9f2f09bab775663568430905ef69010
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-09-25 10:42:36 +0100

    Add token

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,30 @@
+mod lex;
 pub mod vcs;
+
+/// Let's start with defining all kinds of tokens and
+/// composite nodes.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[allow(non_camel_case_types)]
+#[repr(u16)]
+pub(crate) enum SyntaxKind {
+    KEY = 0,
+    VALUE,
+    COLON,
+    INDENT,
+    NEWLINE,
+    WHITESPACE, // whitespaces is explicit
+    COMMENT,    // comments
+    ERROR,      // as well as errors
+
+    // composite nodes
+    ROOT,      // The entire file
+    PARAGRAPH, // A deb822 paragraph
+    ENTRY,     // A single key-value pair
+}
+
+/// Convert our `SyntaxKind` into the rowan `SyntaxKind`.
+impl From<SyntaxKind> for rowan::SyntaxKind {
+    fn from(kind: SyntaxKind) -> Self {
+        Self(kind as u16)
+    }
+}
