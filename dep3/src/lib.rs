103ecd26f2c8ec2691197f7457cd19e1670f4b08
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-11-28 15:27:50 +0000

    Add crate description

--- dep3/src/lib.rs
+++ dep3/src/lib.rs
@@ -1,3 +1,22 @@
+//! A library for parsing and generating Debian patch headers.
+//!
+//! # Examples
+//!
+//! ```rust
+//! use dep3::PatchHeader;
+//! use std::str::FromStr;
+//! let text = r#"From: John Doe <john.doe@example>
+//! Date: Mon, 1 Jan 2000 00:00:00 +0000
+//! Subject: [PATCH] fix a bug
+//! Bug-Debian: https://bugs.debian.org/123456
+//! Bug: https://bugzilla.example.com/bug.cgi?id=123456
+//! Forwarded: not-needed
+//! "#;
+//!
+//! let patch_header = PatchHeader::from_str(text).unwrap();
+//! assert_eq!(patch_header.description(), Some("[PATCH] fix a bug".to_string()));
+//! assert_eq!(patch_header.vendor_bugs("Debian").collect::<Vec<_>>(), vec!["https://bugs.debian.org/123456".to_string()]);
+//! ```
 use deb822_lossless::Paragraph;
 use std::str::FromStr;
 
@@ -201,6 +220,16 @@ impl PatchHeader {
                 Some((Some(k.strip_prefix("Bug-").unwrap().to_string()), v))
             } else if k == "Bug" {
                 Some((None, v))
+            } else {
+                None
+            }
+        })
+    }
+
+    pub fn vendor_bugs<'a>(&'a self, vendor: &'a str) -> impl Iterator<Item = String> + '_ {
+        self.bugs().filter_map(|(k, v)| {
+            if k == Some(vendor.to_string()) {
+                Some(v)
             } else {
                 None
             }
