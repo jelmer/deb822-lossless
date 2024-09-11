9ca071eb409f60c45727007ba1bed5bcec27a311
Jelmer Vernooij <jelmer@jelmer.uk>
2024-09-11 20:33:30 +0000

    Add more docstrings

--- debian-control/src/lossless/mod.rs
+++ debian-control/src/lossless/mod.rs
@@ -1,3 +1,9 @@
+//! Lossless parser for various Debian control files
+//!
+//! This library provides a parser for various Debian control files, such as `control`, `changes`,
+//! and apt `Release`, `Packages`, and `Sources` files. The parser is lossless, meaning that it
+//! preserves all formatting as well as any possible errors in the files.
+
 pub mod apt;
 pub mod changes;
 pub mod control;
