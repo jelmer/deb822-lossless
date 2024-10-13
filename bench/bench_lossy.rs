39c5b83981d9b3a52195eceaba40509da26b5b5d
Jelmer Vernooij <jelmer@jelmer.uk>
2024-10-13 13:48:28 +0000

    Add benchmarks

---
+++ bench/bench_lossy.rs
@@ -0,0 +1,17 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use deb822_lossless::lossy::Deb822;
+
+fn parse_deb822_benchmark(c: &mut Criterion) {
+    let control_data =
+        std::fs::read_to_string(concat!(env!("CARGO_MANIFEST_DIR"), "/bench/Sources"))
+            .expect("Could not read control file");
+
+    c.bench_function("parse_deb822_lossy", |b| {
+        b.iter(|| {
+            let deb822: Deb822 = control_data.parse().unwrap();
+        });
+    });
+}
+
+criterion_group!(benches, parse_deb822_benchmark);
+criterion_main!(benches);
