6ed45ebeb7ca6d19856c7bdef03c78cb44ab782e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-11-13 12:58:24 +0000

    Include README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,6 @@
 #![deny(missing_docs)]
 #![allow(clippy::type_complexity)]
-//! Parser for deb822 style files.
-//!
-//! This parser can be used to parse files in the deb822 format, while preserving
-//! all whitespace and comments. It is based on the [rowan] library, which is a
-//! lossless parser library for Rust.
-//!
-//! Once parsed, the file can be traversed or modified, and then written back to
-//! a file.
-//!
-//! # Example
-//!
-//! ```rust
-//! use deb822_lossless::Deb822;
-//! use std::str::FromStr;
-//!
-//! let input = r#"Package: deb822-lossless
-//! Maintainer: Jelmer Vernooĳ <jelmer@debian.org>
-//! Homepage: https://github.com/jelmer/deb822-lossless
-//! Section: rust
-//!
-//! Package: deb822-lossless
-//! Architecture: any
-//! Description: Lossless parser for deb822 style files.
-//!   This parser can be used to parse files in the deb822 format, while preserving
-//!   all whitespace and comments. It is based on the [rowan] library, which is a
-//!   lossless parser library for Rust.
-//! "#;
-//!
-//! let deb822 = Deb822::from_str(input).unwrap();
-//! assert_eq!(deb822.paragraphs().count(), 2);
-//! let homepage = deb822.paragraphs().nth(0).unwrap().get("Homepage");
-//! assert_eq!(homepage.as_deref(), Some("https://github.com/jelmer/deb822-lossless"));
-//! ```
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 
 mod common;
 pub mod convert;
