a211caf46bc84ac30168da77f3c3eec6215e8ff5
Michał Fita <4925040+michalfita@users.noreply.github.com>
2024-11-20 20:02:05 +0000

    Adjust content of `Repository` to specification from the man page

--- apt-source/src/lib.rs
+++ apt-source/src/lib.rs
@@ -82,6 +82,47 @@ impl ToString for RepositoryType {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
+/// Enumeration for fields like `By-Hash` which have third value of `force`
+pub enum YesNoForce {
+    /// True
+    Yes,
+    /// False
+    No,
+    /// Forced
+    Force
+}
+
+
+impl FromStr for YesNoForce {
+    type Err = RepositoryError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "yes" => Ok(Self::Yes),
+            "no" => Ok(Self::No),
+            "force" => Ok(Self::Force),
+            _ => Err(RepositoryError::InvalidType)
+        }
+    }
+}
+
+impl From<&YesNoForce> for String {
+    fn from(value: &YesNoForce) -> Self {
+        match value {
+            YesNoForce::Yes => "yes".to_owned(),
+            YesNoForce::No => "no".to_owned(),
+            YesNoForce::Force => "force".to_owned()
+        }
+    }
+}
+
+impl ToString for &YesNoForce {
+    fn to_string(&self) -> String {
+        self.to_owned().into()
+    }
+}
+
 fn deserialize_types(text: &str) -> Result<HashSet<RepositoryType>, RepositoryError> {
     text.split_whitespace()
         .map(|t| RepositoryType::from_str(t))
@@ -109,30 +150,121 @@ fn deserialize_string_chain(text: &str) -> Result<Vec<String>, String> { // TODO
         .collect())
 }
 
+fn deserialize_yesno(text: &str) -> Result<bool, String> { // TODO: bad error type
+    match text {
+        "yes" => Ok(true),
+        "no" => Ok(false),
+        _ => Err("Invalid value for yes/no field".to_owned())
+    }
+}
+
+fn serializer_yesno(value: &bool) -> String {
+    if *value {
+        "yes".to_owned()
+    } else {
+        "no".to_owned()
+    }
+}
+
 fn serialize_string_chain(chain: &[String]) -> String {
     chain.join(" ")
 }
 
 /// A structure representing APT repository as declared by DEB822 source file
-#[derive(FromDeb822, ToDeb822, Clone, PartialEq, /*Eq,*/ Debug)]
+/// 
+/// According to `sources.list(5)` man pages, only four fields are mandatory:
+/// * `Types` either `deb` or/and `deb-src`
+/// * `URIs` to repositories holding valid APT structure (unclear if multiple are allowed)
+/// * `Suites` usually being distribution codenames
+/// * `Component` most of the time `main`, but it's a section of the repository
+/// 
+/// The manpage specifies following optional fields
+/// * `Enabled`        is a yes/no field, default yes
+/// * `Architectures`
+/// * `Languages`
+/// * `Targets`
+/// * `PDiffs`         is a yes/no field
+/// * `By-Hash`        is a yes/no/force field
+/// * `Allow-Insecure` is a yes/no field, default no
+/// * `Allow-Weak`     is a yes/no field, default no
+/// * `Allow-Downgrade-To-Insecure` is a yes/no field, default no
+/// * `Trusted`        us a yes/no field
+/// * `Signed-By`      is either path to the key or PGP key block
+/// * `Check-Valid-Until` is a yes/no field
+/// * `Valid-Until-Min`
+/// * `Valid-Until-Max`
+/// * `Check-Date`     is a yes/no field
+/// * `Date-Max-Future`
+/// * `InRelease-Path` relative path
+/// * `Snapshot`       either `enable` or a snapshot ID
+/// 
+/// The unit tests of APT use:
+/// * `Description`
+/// 
+/// The RepoLib tool uses:
+/// * `X-Repolib-Name` identifier for own reference, meaningless for APT
+/// 
+/// Note: Multivalues `*-Add` & `*-Remove` semantics aren't supported.
+#[derive(FromDeb822, ToDeb822, Clone, PartialEq, /*Eq,*/ Debug, Default)]
 pub struct Repository {
-    #[deb822(field = "Enabled")] // TODO: support for `default` if omitted is missing
+    /// If `no` (false) the repository is ignored by APT
+    #[deb822(field = "Enabled", deserialize_with = deserialize_yesno, serialize_with = serializer_yesno)] // TODO: support for `default` if omitted is missing
     enabled: Option<bool>,
+
+    /// The value `RepositoryType::Binary` (`deb`) or/and `RepositoryType::Source` (`deb-src`)
     #[deb822(field = "Types", deserialize_with = deserialize_types, serialize_with = serialize_types)]
     types: HashSet<RepositoryType>, // consider alternative, closed set
-    #[deb822(field = "Architectures", deserialize_with = deserialize_string_chain, serialize_with = serialize_string_chain)]
-    architectures: Vec<String>,
+    /// The address of the repository
     #[deb822(field = "URIs", deserialize_with = deserialize_uris, serialize_with = serialize_uris)]
     uris: Vec<Url>, // according to Debian that's URI, but this type is more advanced than URI from `http` crate
+    /// The distribution name as codename or suite type (like `stable` or `testing`)
     #[deb822(field = "Suites", deserialize_with = deserialize_string_chain, serialize_with = serialize_string_chain)]
     suites: Vec<String>,
+    /// Section of the repository, usually `main`, `contrib` or `non-free`
     #[deb822(field = "Components", deserialize_with = deserialize_string_chain, serialize_with = serialize_string_chain)]
     components: Vec<String>,
+
+    /// (Optional) Architectures binaries from this repository run on
+    #[deb822(field = "Architectures", deserialize_with = deserialize_string_chain, serialize_with = serialize_string_chain)]
+    architectures: Vec<String>,
+    /// (Optional) Translations support to download
+    #[deb822(field = "Languages", deserialize_with = deserialize_string_chain, serialize_with = serialize_string_chain)]
+    languages: Option<Vec<String>>, // TODO: Option is redundant to empty vectors
+    /// (Optional) Download targets to acquire from this source
+    #[deb822(field = "Targets", deserialize_with = deserialize_string_chain, serialize_with = serialize_string_chain)]
+    targets: Option<Vec<String>>,
+    /// (Optional) Controls if APT should try PDiffs instead of downloading indexes entirely; if not set defaults to configuration option `Acquire::PDiffs`
+    #[deb822(field = "PDiffs", deserialize_with = deserialize_yesno)]
+    pdiffs: Option<bool>,
+    /// (Optional) Controls if APT should try to acquire indexes via a URI constructed from a hashsum of the expected file
+    #[deb822(field = "By-Hash")]
+    by_hash: Option<YesNoForce>,
+    /// (Optional) If yes circumvents parts of `apt-secure`, don't thread lightly
+    #[deb822(field = "Allow-Insecure")]
+    allow_insecure: Option<bool>, // TODO: redundant option, not present = default no
+    /// (Optional) If yes circumvents parts of `apt-secure`, don't thread lightly
+    #[deb822(field = "Allow-Weak")]
+    allow_weak: Option<bool>, // TODO: redundant option, not present = default no
+    /// (Optional) If yes circumvents parts of `apt-secure`, don't thread lightly
+    #[deb822(field = "Allow-Downgrade-To-Insecure")]
+    allow_downgrade_to_insecure: Option<bool>, // TODO: redundant option, not present = default no
+    /// (Optional) If set forces whether APT considers source as rusted or no (default not present is a third state)
+    #[deb822(field = "Trusted")]
+    trusted: Option<bool>,
+    /// (Optional) Contains either absolute path to GPG keyring or embedded GPG public key block, if not set APT uses all trusted keys;
+    /// I can't find example of using with fingerprints
     #[deb822(field = "Signed-By")]
     signature: Option<Signature>,
+
+    /// (Optional) Field ignored by APT but used by RepoLib to identify repositories, Ubuntu sources contain them
     #[deb822(field = "X-Repolib-Name")]
     x_repolib_name: Option<String>, // this supports RepoLib still used by PopOS, even if removed from Debian/Ubuntu
-    //options: HashMap<String, String>
+
+    /// (Optional) Field not present in the man page, but used in APT unit tests, potentially to hold the repository description
+    #[deb822(field = "Description")]
+    description: Option<String>
+
+    // options: HashMap<String, String> // My original parser kept remaining optional fields in the hash map, is this right approach?
 }
 
 impl Repository {
@@ -267,16 +399,20 @@ mod tests {
                 components: vec!["main". to_owned()],
                 signature: None,
                 x_repolib_name: None,
+                languages: None,
+                targets: None,
+                pdiffs: None,
+                ..Default::default()
             }
         ]);
         let text = repos.to_string();
         assert_eq!(text, indoc! {r#"
             Enabled: yes
             Types: deb
-            Architectures: arm64
-            URIs: https://deb/debian.org/debian
+            URIs: https://deb.debian.org/debian
             Suites: jammy
             Components: main
+            Architectures: arm64
         "#});
     }
 }
